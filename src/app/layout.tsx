7ab1a1e8876a8ccdcd1a4fff19d780f925404d7e
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-12-05 20:35:03 +0530

    feat:add google anaylitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0ZTDG4LDTS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0ZTDG4LDTS');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
